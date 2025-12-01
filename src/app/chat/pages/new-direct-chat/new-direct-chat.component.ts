aff7ac9e2ff540ba2b71eaad7de79c003f2ba03b
Sylwia <sylwia@LPL-5CG5373LYD.corp.capgemini.com>
2025-12-01 15:02:16 +0100

    feat: create direct chat

---
+++ src/app/chat/pages/new-direct-chat/new-direct-chat.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { ChatHeaderComponent } from '../../shared/components/chat-header/chat-header.component';
+import { ChatNewContentComponent } from '../../shared/components/new-content-chat/new-content-chat.component';
+
+@Component({
+  selector: 'app-new-direct-chat',
+  templateUrl: './new-direct-chat.component.html',
+  styleUrls: ['./new-direct-chat.component.scss'],
+  standalone: true,
+  imports: [FormsModule, ChatHeaderComponent, ChatNewContentComponent],
+})
+export class NewDirectChatComponent {
+  @Output() back = new EventEmitter<void>();
+
+  private recipientInputSubject = new BehaviorSubject<string>('');
+  
+  chatName$ = new BehaviorSubject<string>('Direct Chat');
+  recipients$ = new BehaviorSubject<string[]>([]);
+  recipientInput$ = this.recipientInputSubject.asObservable();
+
+  onRecipientInputChange(event: Event) {
+    const value = (event.target as HTMLInputElement)?.value ?? '';
+    this.recipientInputSubject.next(value);
+  }
+
+  onAddRecipient() {}
+  
+  onRemoveRecipient(index: number) {}
+}
